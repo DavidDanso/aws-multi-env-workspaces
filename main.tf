b3a3cb49507aa9a9377615b4976e874312b558aa
DavidDanso <desiingerboy@icloud.com>
2026-04-14 11:16:24 +0000

    refactor: rename networking, security, and compute modules to vpc, security_group, and ec2 respectively

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-module "networking" {
-  source               = "./modules/networking"
+module "vpc" {
+  source               = "./modules/vpc"
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   availability_zones   = var.availability_zones
 }
 
-module "security" {
-  source = "./modules/security"
-  vpc_id = module.networking.vpc_id
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id = module.vpc.vpc_id
 }
 
-module "compute" {
-  source            = "./modules/compute"
+module "ec2" {
+  source            = "./modules/ec2"
   instance_count    = var.instance_count
   ami_id            = var.ami_id
   instance_type     = var.instance_type
   key_name          = var.key_name
-  subnet_id         = module.networking.public_subnet_ids[0]
+  subnet_id         = module.vpc.public_subnet_ids[0]
   security_group_id = module.security.security_group_id
 }
